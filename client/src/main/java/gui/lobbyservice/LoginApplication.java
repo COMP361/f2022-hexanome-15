6e6b32d36c368ec7557abc3be635fd11e354cee9
Sora XO <40028513+SoraXO@users.noreply.github.com>
2022-10-28 16:40:52 -0400

    Re-commit for lobbyservice frontend
    Re-committing the lobby service user interface applications because of a staging error in the previous commit

---
+++ client/src/main/java/gui/lobbyservice/LoginApplication.java
@@ -0,0 +1,23 @@
+package com.lobbyservice.lobbyservice;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class LoginApplication extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(LoginApplication.class.getResource("hello-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 1000, 700);
+        stage.setTitle("Login or Sign Up");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
