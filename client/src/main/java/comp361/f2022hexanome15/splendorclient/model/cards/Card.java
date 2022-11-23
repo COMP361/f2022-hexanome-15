d29b496b61286a2838a9d0c5fc6b1ad350f87949
ZacharyHayden <zachary.hayden@mail.mcgill.ca>
2022-11-23 13:53:45 -0500

    updated card, deck, tokenpile via Sofia

--- client/src/main/java/comp361/f2022hexanome15/splendorclient/model/cards/Card.java
+++ client/src/main/java/comp361/f2022hexanome15/splendorclient/model/cards/Card.java
@@ -3,6 +3,7 @@ package comp361.f2022hexanome15.splendorclient.model.cards;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Objects;
 
 import comp361.f2022hexanome15.splendorclient.model.tokens.TokenType;
 import javafx.scene.paint.Color;
@@ -31,6 +32,9 @@ public class Card {
 	 * @param id         The id of the card
 	 * @param prestige   The prestige earned upon purchasing the card
 	 * @param tokenBonus The bonus received upon purchasing the card
+	 * @param cardType   The type of card
+	 * @param discount   The card's discount
+	 * @param cost       The cost of the card
 	 */
 	public Card(int id, int prestige, TokenType tokenBonus, CardType cardType, int discount, int[] cost) {
 		this.id = id;
@@ -132,17 +136,17 @@ public class Card {
 	 * Generates cards for the decks.
 	 */
 	private static void generateCards() {
-		cards.add(new Card(0, 1, TokenType.DIAMOND, CardType.BASE1, 1, new int[] { 0, 0, 4, 0, 0 }));
-		cards.add(new Card(1, 1, TokenType.SAPPHIRE, CardType.BASE1, 1, new int[] { 0, 0, 0, 4, 0 }));
-		cards.add(new Card(2, 1, TokenType.EMERALD, CardType.BASE1, 1, new int[] { 0, 0, 0, 0, 4 }));
-		cards.add(new Card(3, 1, TokenType.RUBY, CardType.BASE1, 1, new int[] { 4, 0, 0, 0, 0 }));
-		cards.add(new Card(4, 1, TokenType.ONYX, CardType.BASE1, 1, new int[] { 0, 4, 0, 0, 0 }));
+		cards.add(new Card(0, 1, TokenType.DIAMOND, CardType.BASE1, 1, new int[] { 0, 0, 1, 0, 0 }));
+		cards.add(new Card(1, 1, TokenType.SAPPHIRE, CardType.BASE1, 1, new int[] { 0, 0, 0, 1, 0 }));
+		cards.add(new Card(2, 1, TokenType.EMERALD, CardType.BASE1, 1, new int[] { 0, 0, 0, 0, 1 }));
+		cards.add(new Card(3, 1, TokenType.RUBY, CardType.BASE1, 1, new int[] { 1, 0, 0, 0, 0 }));
+		cards.add(new Card(4, 1, TokenType.ONYX, CardType.BASE1, 1, new int[] { 0, 1, 0, 0, 0 }));
 
-		cards.add(new Card(5, 1, TokenType.DIAMOND, CardType.BASE1, 1, new int[] { 0, 0, 4, 0, 0 }));
-		cards.add(new Card(6, 1, TokenType.SAPPHIRE, CardType.BASE1, 1, new int[] { 0, 0, 0, 4, 0 }));
-		cards.add(new Card(7, 1, TokenType.EMERALD, CardType.BASE1, 1, new int[] { 0, 0, 0, 0, 4 }));
-		cards.add(new Card(8, 1, TokenType.RUBY, CardType.BASE1, 1, new int[] { 4, 0, 0, 0, 0 }));
-		cards.add(new Card(9, 1, TokenType.ONYX, CardType.BASE1, 1, new int[] { 0, 4, 0, 0, 0 }));
+		cards.add(new Card(5, 1, TokenType.DIAMOND, CardType.BASE1, 1, new int[] { 0, 0, 1, 0, 0 }));
+		cards.add(new Card(6, 1, TokenType.SAPPHIRE, CardType.BASE1, 1, new int[] { 0, 0, 0, 1, 0 }));
+		cards.add(new Card(7, 1, TokenType.EMERALD, CardType.BASE1, 1, new int[] { 0, 0, 0, 0, 1 }));
+		cards.add(new Card(8, 1, TokenType.RUBY, CardType.BASE1, 1, new int[] { 1, 0, 0, 0, 0 }));
+		cards.add(new Card(9, 1, TokenType.ONYX, CardType.BASE1, 1, new int[] { 0, 1, 0, 0, 0 }));
 
 		cards.add(new Card(10, 1, TokenType.DIAMOND, CardType.BASE1, 1, new int[] { 0, 0, 4, 0, 0 }));
 		cards.add(new Card(11, 1, TokenType.SAPPHIRE, CardType.BASE1, 1, new int[] { 0, 0, 0, 4, 0 }));
@@ -150,95 +154,112 @@ public class Card {
 		cards.add(new Card(13, 1, TokenType.RUBY, CardType.BASE1, 1, new int[] { 4, 0, 0, 0, 0 }));
 		cards.add(new Card(14, 1, TokenType.ONYX, CardType.BASE1, 1, new int[] { 0, 4, 0, 0, 0 }));
 
-		cards.add(new Card(15, 1, TokenType.DIAMOND, CardType.BASE1, 1, new int[] { 0, 0, 4, 0, 0 }));
-		cards.add(new Card(16, 1, TokenType.SAPPHIRE, CardType.BASE1, 1, new int[] { 0, 0, 0, 4, 0 }));
-		cards.add(new Card(17, 1, TokenType.EMERALD, CardType.BASE1, 1, new int[] { 0, 0, 0, 0, 4 }));
-		cards.add(new Card(18, 1, TokenType.RUBY, CardType.BASE1, 1, new int[] { 4, 0, 0, 0, 0 }));
-		cards.add(new Card(19, 1, TokenType.ONYX, CardType.BASE1, 1, new int[] { 0, 4, 0, 0, 0 }));
+		cards.add(new Card(15, 1, TokenType.DIAMOND, CardType.BASE1, 1, new int[] { 0, 0, 1, 0, 0 }));
+		cards.add(new Card(16, 1, TokenType.SAPPHIRE, CardType.BASE1, 1, new int[] { 0, 0, 0, 1, 0 }));
+		cards.add(new Card(17, 1, TokenType.EMERALD, CardType.BASE1, 1, new int[] { 0, 0, 0, 0, 1 }));
+		cards.add(new Card(18, 1, TokenType.RUBY, CardType.BASE1, 1, new int[] { 1, 0, 0, 0, 0 }));
+		cards.add(new Card(19, 1, TokenType.ONYX, CardType.BASE1, 1, new int[] { 0, 1, 0, 0, 0 }));
 
-		cards.add(new Card(20, 1, TokenType.DIAMOND, CardType.BASE1, 1, new int[] { 0, 0, 4, 0, 0 }));
-		cards.add(new Card(21, 1, TokenType.SAPPHIRE, CardType.BASE1, 1, new int[] { 0, 0, 0, 4, 0 }));
-		cards.add(new Card(22, 1, TokenType.EMERALD, CardType.BASE1, 1, new int[] { 0, 0, 0, 0, 4 }));
-		cards.add(new Card(23, 1, TokenType.RUBY, CardType.BASE1, 1, new int[] { 4, 0, 0, 0, 0 }));
-		cards.add(new Card(24, 1, TokenType.ONYX, CardType.BASE1, 1, new int[] { 0, 4, 0, 0, 0 }));
+		cards.add(new Card(20, 1, TokenType.DIAMOND, CardType.BASE1, 1, new int[] { 0, 0, 1, 0, 0 }));
+		cards.add(new Card(21, 1, TokenType.SAPPHIRE, CardType.BASE1, 1, new int[] { 0, 0, 0, 1, 0 }));
+		cards.add(new Card(22, 1, TokenType.EMERALD, CardType.BASE1, 1, new int[] { 0, 0, 0, 0, 1 }));
+		cards.add(new Card(23, 1, TokenType.RUBY, CardType.BASE1, 1, new int[] { 1, 0, 0, 0, 0 }));
+		cards.add(new Card(24, 1, TokenType.ONYX, CardType.BASE1, 1, new int[] { 0, 1, 0, 0, 0 }));
 
-		cards.add(new Card(25, 1, TokenType.DIAMOND, CardType.BASE1, 1, new int[] { 0, 0, 4, 0, 0 }));
-		cards.add(new Card(26, 1, TokenType.SAPPHIRE, CardType.BASE1, 1, new int[] { 0, 0, 0, 4, 0 }));
-		cards.add(new Card(27, 1, TokenType.EMERALD, CardType.BASE1, 1, new int[] { 0, 0, 0, 0, 4 }));
-		cards.add(new Card(28, 1, TokenType.RUBY, CardType.BASE1, 1, new int[] { 4, 0, 0, 0, 0 }));
-		cards.add(new Card(29, 1, TokenType.ONYX, CardType.BASE1, 1, new int[] { 0, 4, 0, 0, 0 }));
+		cards.add(new Card(25, 1, TokenType.DIAMOND, CardType.BASE1, 1, new int[] { 0, 0, 1, 0, 0 }));
+		cards.add(new Card(26, 1, TokenType.SAPPHIRE, CardType.BASE1, 1, new int[] { 0, 0, 0, 1, 0 }));
+		cards.add(new Card(27, 1, TokenType.EMERALD, CardType.BASE1, 1, new int[] { 0, 0, 0, 0, 1 }));
+		cards.add(new Card(28, 1, TokenType.RUBY, CardType.BASE1, 1, new int[] { 1, 0, 0, 0, 0 }));
+		cards.add(new Card(29, 1, TokenType.ONYX, CardType.BASE1, 1, new int[] { 0, 1, 0, 0, 0 }));
 
-		cards.add(new Card(30, 1, TokenType.DIAMOND, CardType.BASE1, 1, new int[] { 0, 0, 4, 0, 0 }));
-		cards.add(new Card(31, 1, TokenType.SAPPHIRE, CardType.BASE1, 1, new int[] { 0, 0, 0, 4, 0 }));
-		cards.add(new Card(32, 1, TokenType.EMERALD, CardType.BASE1, 1, new int[] { 0, 0, 0, 0, 4 }));
-		cards.add(new Card(33, 1, TokenType.RUBY, CardType.BASE1, 1, new int[] { 4, 0, 0, 0, 0 }));
-		cards.add(new Card(34, 1, TokenType.ONYX, CardType.BASE1, 1, new int[] { 0, 4, 0, 0, 0 }));
+		cards.add(new Card(30, 1, TokenType.DIAMOND, CardType.BASE1, 1, new int[] { 0, 0, 1, 0, 0 }));
+		cards.add(new Card(31, 1, TokenType.SAPPHIRE, CardType.BASE1, 1, new int[] { 0, 0, 0, 1, 0 }));
+		cards.add(new Card(32, 1, TokenType.EMERALD, CardType.BASE1, 1, new int[] { 0, 0, 0, 0, 1 }));
+		cards.add(new Card(33, 1, TokenType.RUBY, CardType.BASE1, 1, new int[] { 1, 0, 0, 0, 0 }));
+		cards.add(new Card(34, 1, TokenType.ONYX, CardType.BASE1, 1, new int[] { 0, 1, 0, 0, 0 }));
 
-		cards.add(new Card(35, 1, TokenType.DIAMOND, CardType.BASE1, 1, new int[] { 0, 0, 4, 0, 0 }));
-		cards.add(new Card(36, 1, TokenType.SAPPHIRE, CardType.BASE1, 1, new int[] { 0, 0, 0, 4, 0 }));
-		cards.add(new Card(37, 1, TokenType.EMERALD, CardType.BASE1, 1, new int[] { 0, 0, 0, 0, 4 }));
-		cards.add(new Card(38, 1, TokenType.RUBY, CardType.BASE1, 1, new int[] { 4, 0, 0, 0, 0 }));
-		cards.add(new Card(39, 1, TokenType.ONYX, CardType.BASE1, 1, new int[] { 0, 4, 0, 0, 0 }));
+		cards.add(new Card(35, 1, TokenType.DIAMOND, CardType.BASE1, 1, new int[] { 0, 0, 1, 0, 0 }));
+		cards.add(new Card(36, 1, TokenType.SAPPHIRE, CardType.BASE1, 1, new int[] { 0, 0, 0, 1, 0 }));
+		cards.add(new Card(37, 1, TokenType.EMERALD, CardType.BASE1, 1, new int[] { 0, 0, 0, 0, 1 }));
+		cards.add(new Card(38, 1, TokenType.RUBY, CardType.BASE1, 1, new int[] { 1, 0, 0, 0, 0 }));
+		cards.add(new Card(39, 1, TokenType.ONYX, CardType.BASE1, 1, new int[] { 0, 1, 0, 0, 0 }));
 
-		cards.add(new Card(40, 1, TokenType.DIAMOND, CardType.BASE2, 1, new int[] { 0, 0, 4, 0, 0 }));
-		cards.add(new Card(41, 1, TokenType.SAPPHIRE, CardType.BASE2, 1, new int[] { 0, 0, 0, 4, 0 }));
-		cards.add(new Card(42, 1, TokenType.EMERALD, CardType.BASE2, 1, new int[] { 0, 0, 0, 0, 4 }));
-		cards.add(new Card(43, 1, TokenType.RUBY, CardType.BASE2, 1, new int[] { 4, 0, 0, 0, 0 }));
-		cards.add(new Card(44, 1, TokenType.ONYX, CardType.BASE2, 1, new int[] { 0, 4, 0, 0, 0 }));
+		cards.add(new Card(40, 1, TokenType.DIAMOND, CardType.BASE2, 1, new int[] { 0, 0, 1, 0, 0 }));
+		cards.add(new Card(41, 1, TokenType.SAPPHIRE, CardType.BASE2, 1, new int[] { 0, 0, 0, 1, 0 }));
+		cards.add(new Card(42, 1, TokenType.EMERALD, CardType.BASE2, 1, new int[] { 0, 0, 0, 0, 1 }));
+		cards.add(new Card(43, 1, TokenType.RUBY, CardType.BASE2, 1, new int[] { 1, 0, 0, 0, 0 }));
+		cards.add(new Card(44, 1, TokenType.ONYX, CardType.BASE2, 1, new int[] { 0, 1, 0, 0, 0 }));
 
-		cards.add(new Card(45, 1, TokenType.DIAMOND, CardType.BASE2, 1, new int[] { 0, 0, 4, 0, 0 }));
-		cards.add(new Card(46, 1, TokenType.SAPPHIRE, CardType.BASE2, 1, new int[] { 0, 0, 0, 4, 0 }));
-		cards.add(new Card(47, 1, TokenType.EMERALD, CardType.BASE2, 1, new int[] { 0, 0, 0, 0, 4 }));
-		cards.add(new Card(48, 1, TokenType.RUBY, CardType.BASE2, 1, new int[] { 4, 0, 0, 0, 0 }));
-		cards.add(new Card(49, 1, TokenType.ONYX, CardType.BASE2, 1, new int[] { 0, 4, 0, 0, 0 }));
+		cards.add(new Card(45, 1, TokenType.DIAMOND, CardType.BASE2, 1, new int[] { 0, 0, 1, 0, 0 }));
+		cards.add(new Card(46, 1, TokenType.SAPPHIRE, CardType.BASE2, 1, new int[] { 0, 0, 0, 1, 0 }));
+		cards.add(new Card(47, 1, TokenType.EMERALD, CardType.BASE2, 1, new int[] { 0, 0, 0, 0, 1 }));
+		cards.add(new Card(48, 1, TokenType.RUBY, CardType.BASE2, 1, new int[] { 1, 0, 0, 0, 0 }));
+		cards.add(new Card(49, 1, TokenType.ONYX, CardType.BASE2, 1, new int[] { 0, 1, 0, 0, 0 }));
 
-		cards.add(new Card(50, 1, TokenType.DIAMOND, CardType.BASE2, 1, new int[] { 0, 0, 4, 0, 0 }));
-		cards.add(new Card(51, 1, TokenType.SAPPHIRE, CardType.BASE2, 1, new int[] { 0, 0, 0, 4, 0 }));
-		cards.add(new Card(52, 1, TokenType.EMERALD, CardType.BASE2, 1, new int[] { 0, 0, 0, 0, 4 }));
-		cards.add(new Card(53, 1, TokenType.RUBY, CardType.BASE2, 1, new int[] { 4, 0, 0, 0, 0 }));
-		cards.add(new Card(54, 1, TokenType.ONYX, CardType.BASE2, 1, new int[] { 0, 4, 0, 0, 0 }));
+		cards.add(new Card(50, 1, TokenType.DIAMOND, CardType.BASE2, 1, new int[] { 0, 0, 1, 0, 0 }));
+		cards.add(new Card(51, 1, TokenType.SAPPHIRE, CardType.BASE2, 1, new int[] { 0, 0, 0, 1, 0 }));
+		cards.add(new Card(52, 1, TokenType.EMERALD, CardType.BASE2, 1, new int[] { 0, 0, 0, 0, 1 }));
+		cards.add(new Card(53, 1, TokenType.RUBY, CardType.BASE2, 1, new int[] { 1, 0, 0, 0, 0 }));
+		cards.add(new Card(54, 1, TokenType.ONYX, CardType.BASE2, 1, new int[] { 0, 1, 0, 0, 0 }));
 
-		cards.add(new Card(55, 1, TokenType.DIAMOND, CardType.BASE2, 1, new int[] { 0, 0, 4, 0, 0 }));
-		cards.add(new Card(56, 1, TokenType.SAPPHIRE, CardType.BASE2, 1, new int[] { 0, 0, 0, 4, 0 }));
+		cards.add(new Card(55, 1, TokenType.DIAMOND, CardType.BASE2, 1, new int[] { 0, 0, 1, 0, 0 }));
+		cards.add(new Card(56, 1, TokenType.SAPPHIRE, CardType.BASE2, 1, new int[] { 0, 0, 0, 1, 0 }));
 		cards.add(new Card(57, 1, TokenType.EMERALD, CardType.BASE2, 1, new int[] { 0, 0, 0, 0, 4 }));
-		cards.add(new Card(58, 1, TokenType.RUBY, CardType.BASE2, 1, new int[] { 4, 0, 0, 0, 0 }));
-		cards.add(new Card(59, 1, TokenType.ONYX, CardType.BASE2, 1, new int[] { 0, 4, 0, 0, 0 }));
+		cards.add(new Card(58, 1, TokenType.RUBY, CardType.BASE2, 1, new int[] { 1, 0, 0, 0, 0 }));
+		cards.add(new Card(59, 1, TokenType.ONYX, CardType.BASE2, 1, new int[] { 0, 1, 0, 0, 0 }));
 
-		cards.add(new Card(60, 1, TokenType.DIAMOND, CardType.BASE2, 1, new int[] { 0, 0, 4, 0, 0 }));
-		cards.add(new Card(61, 1, TokenType.SAPPHIRE, CardType.BASE2, 1, new int[] { 0, 0, 0, 4, 0 }));
-		cards.add(new Card(62, 1, TokenType.EMERALD, CardType.BASE2, 1, new int[] { 0, 0, 0, 0, 4 }));
-		cards.add(new Card(63, 1, TokenType.RUBY, CardType.BASE2, 1, new int[] { 4, 0, 0, 0, 0 }));
-		cards.add(new Card(64, 1, TokenType.ONYX, CardType.BASE2, 1, new int[] { 0, 4, 0, 0, 0 }));
+		cards.add(new Card(60, 1, TokenType.DIAMOND, CardType.BASE2, 1, new int[] { 0, 0, 1, 0, 0 }));
+		cards.add(new Card(61, 1, TokenType.SAPPHIRE, CardType.BASE2, 1, new int[] { 0, 0, 0, 1, 0 }));
+		cards.add(new Card(62, 1, TokenType.EMERALD, CardType.BASE2, 1, new int[] { 0, 0, 0, 0, 1 }));
+		cards.add(new Card(63, 1, TokenType.RUBY, CardType.BASE2, 1, new int[] { 1, 0, 0, 0, 0 }));
+		cards.add(new Card(64, 1, TokenType.ONYX, CardType.BASE2, 1, new int[] { 0, 1, 0, 0, 0 }));
 
-		cards.add(new Card(65, 1, TokenType.DIAMOND, CardType.BASE2, 1, new int[] { 0, 0, 4, 0, 0 }));
-		cards.add(new Card(66, 1, TokenType.SAPPHIRE, CardType.BASE2, 1, new int[] { 0, 0, 0, 4, 0 }));
-		cards.add(new Card(67, 1, TokenType.EMERALD, CardType.BASE2, 1, new int[] { 0, 0, 0, 0, 4 }));
-		cards.add(new Card(68, 1, TokenType.RUBY, CardType.BASE2, 1, new int[] { 4, 0, 0, 0, 0 }));
-		cards.add(new Card(69, 1, TokenType.ONYX, CardType.BASE2, 1, new int[] { 0, 4, 0, 0, 0 }));
+		cards.add(new Card(65, 1, TokenType.DIAMOND, CardType.BASE2, 1, new int[] { 0, 0, 1, 0, 0 }));
+		cards.add(new Card(66, 1, TokenType.SAPPHIRE, CardType.BASE2, 1, new int[] { 0, 0, 0, 1, 0 }));
+		cards.add(new Card(67, 1, TokenType.EMERALD, CardType.BASE2, 1, new int[] { 0, 0, 0, 0, 1 }));
+		cards.add(new Card(68, 1, TokenType.RUBY, CardType.BASE2, 1, new int[] { 1, 0, 0, 0, 0 }));
+		cards.add(new Card(69, 1, TokenType.ONYX, CardType.BASE2, 1, new int[] { 0, 1, 0, 0, 0 }));
 
-		cards.add(new Card(70, 1, TokenType.DIAMOND, CardType.BASE3, 1, new int[] { 0, 0, 4, 0, 0 }));
-		cards.add(new Card(71, 1, TokenType.SAPPHIRE, CardType.BASE3, 1, new int[] { 0, 0, 0, 4, 0 }));
-		cards.add(new Card(72, 1, TokenType.EMERALD, CardType.BASE3, 1, new int[] { 0, 0, 0, 0, 4 }));
-		cards.add(new Card(73, 1, TokenType.RUBY, CardType.BASE3, 1, new int[] { 4, 0, 0, 0, 0 }));
-		cards.add(new Card(74, 1, TokenType.ONYX, CardType.BASE3, 1, new int[] { 0, 4, 0, 0, 0 }));
+		cards.add(new Card(70, 1, TokenType.DIAMOND, CardType.BASE3, 1, new int[] { 0, 0, 1, 0, 0 }));
+		cards.add(new Card(71, 1, TokenType.SAPPHIRE, CardType.BASE3, 1, new int[] { 0, 0, 0, 1, 0 }));
+		cards.add(new Card(72, 1, TokenType.EMERALD, CardType.BASE3, 1, new int[] { 0, 0, 0, 0, 1 }));
+		cards.add(new Card(73, 1, TokenType.RUBY, CardType.BASE3, 1, new int[] { 1, 0, 0, 0, 0 }));
+		cards.add(new Card(74, 1, TokenType.ONYX, CardType.BASE3, 1, new int[] { 0, 1, 0, 0, 0 }));
 
-		cards.add(new Card(75, 1, TokenType.DIAMOND, CardType.BASE3, 1, new int[] { 0, 0, 4, 0, 0 }));
-		cards.add(new Card(76, 1, TokenType.SAPPHIRE, CardType.BASE3, 1, new int[] { 0, 0, 0, 4, 0 }));
-		cards.add(new Card(77, 1, TokenType.EMERALD, CardType.BASE3, 1, new int[] { 0, 0, 0, 0, 4 }));
-		cards.add(new Card(78, 1, TokenType.RUBY, CardType.BASE3, 1, new int[] { 4, 0, 0, 0, 0 }));
-		cards.add(new Card(79, 1, TokenType.ONYX, CardType.BASE3, 1, new int[] { 0, 4, 0, 0, 0 }));
+		cards.add(new Card(75, 1, TokenType.DIAMOND, CardType.BASE3, 1, new int[] { 0, 0, 1, 0, 0 }));
+		cards.add(new Card(76, 1, TokenType.SAPPHIRE, CardType.BASE3, 1, new int[] { 0, 0, 0, 1, 0 }));
+		cards.add(new Card(77, 1, TokenType.EMERALD, CardType.BASE3, 1, new int[] { 0, 0, 0, 0, 1 }));
+		cards.add(new Card(78, 1, TokenType.RUBY, CardType.BASE3, 1, new int[] { 1, 0, 0, 0, 0 }));
+		cards.add(new Card(79, 1, TokenType.ONYX, CardType.BASE3, 1, new int[] { 0, 1, 0, 0, 0 }));
 
-		cards.add(new Card(80, 1, TokenType.DIAMOND, CardType.BASE3, 1, new int[] { 0, 0, 4, 0, 0 }));
-		cards.add(new Card(81, 1, TokenType.SAPPHIRE, CardType.BASE3, 1, new int[] { 0, 0, 0, 4, 0 }));
-		cards.add(new Card(82, 1, TokenType.EMERALD, CardType.BASE3, 1, new int[] { 0, 0, 0, 0, 4 }));
-		cards.add(new Card(83, 1, TokenType.RUBY, CardType.BASE3, 1, new int[] { 4, 0, 0, 0, 0 }));
-		cards.add(new Card(84, 1, TokenType.ONYX, CardType.BASE3, 1, new int[] { 0, 4, 0, 0, 0 }));
+		cards.add(new Card(80, 1, TokenType.DIAMOND, CardType.BASE3, 1, new int[] { 0, 0, 1, 0, 0 }));
+		cards.add(new Card(81, 1, TokenType.SAPPHIRE, CardType.BASE3, 1, new int[] { 0, 0, 0, 1, 0 }));
+		cards.add(new Card(82, 1, TokenType.EMERALD, CardType.BASE3, 1, new int[] { 0, 0, 0, 0, 1 }));
+		cards.add(new Card(83, 1, TokenType.RUBY, CardType.BASE3, 1, new int[] { 1, 0, 0, 0, 0 }));
+		cards.add(new Card(84, 1, TokenType.ONYX, CardType.BASE3, 1, new int[] { 0, 1, 0, 0, 0 }));
 
-		cards.add(new Card(85, 1, TokenType.DIAMOND, CardType.BASE3, 1, new int[] { 0, 0, 4, 0, 0 }));
-		cards.add(new Card(86, 1, TokenType.SAPPHIRE, CardType.BASE3, 1, new int[] { 0, 0, 0, 4, 0 }));
-		cards.add(new Card(87, 1, TokenType.EMERALD, CardType.BASE3, 1, new int[] { 0, 0, 0, 0, 4 }));
-		cards.add(new Card(88, 1, TokenType.RUBY, CardType.BASE3, 1, new int[] { 4, 0, 0, 0, 0 }));
-		cards.add(new Card(89, 1, TokenType.ONYX, CardType.BASE3, 1, new int[] { 0, 4, 0, 0, 0 }));
+		cards.add(new Card(85, 1, TokenType.DIAMOND, CardType.BASE3, 1, new int[] { 0, 0, 1, 0, 0 }));
+		cards.add(new Card(86, 1, TokenType.SAPPHIRE, CardType.BASE3, 1, new int[] { 0, 0, 0, 1, 0 }));
+		cards.add(new Card(87, 1, TokenType.EMERALD, CardType.BASE3, 1, new int[] { 0, 0, 0, 0, 1 }));
+		cards.add(new Card(88, 1, TokenType.RUBY, CardType.BASE3, 1, new int[] { 1, 0, 0, 0, 0 }));
+		cards.add(new Card(89, 1, TokenType.ONYX, CardType.BASE3, 1, new int[] { 0, 1, 0, 0, 0 }));
+	}
+
+	@Override
+	public boolean equals(Object object) {
+		if (object == this) {
+			return true;
+		}
+		if (!(object instanceof Card)) {
+			return false;
+		}
+		Card card = (Card) object;
+		return id == card.getId();
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
 	}
 
 	// TODO: need things like cost, associated gem discount, and prestige.
