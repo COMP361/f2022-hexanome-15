876a26e82edee6f0d92697747f0f1d8a38f45a13
ZacharyHayden <zachary.hayden@mail.mcgill.ca>
2022-11-28 19:08:47 -0500

    updated the game controller

--- server/src/main/java/ca/mcgill/splendorserver/games/GameController.java
+++ server/src/main/java/ca/mcgill/splendorserver/games/GameController.java
@@ -8,6 +8,8 @@ package ca.mcgill.splendorserver.games;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+
+import org.hibernate.annotations.Parent;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -25,90 +27,124 @@ import org.springframework.web.context.request.async.DeferredResult;
  */
 @RestController
 public class GameController {
-  private static final Logger LOGGER = LoggerFactory.getLogger(GameController.class);
-  private final GameRepository repository;
-  private ExecutorService updaters =
-      Executors.newFixedThreadPool(4); // 4 threads for the max 4 players
-  private boolean updateGameBoard = true;
+	private static final Logger LOGGER = LoggerFactory.getLogger(GameController.class);
+	private final GameRepository repository;
+	private ExecutorService updaters = Executors.newFixedThreadPool(4); // 4 threads for the max 4 players
+	private boolean updateGameBoard = false;
+	private boolean updateAction = false;
 
-  public GameController(GameRepository repository) {
-    this.repository = repository;
-  }
+	public GameController(GameRepository repository) {
+		this.repository = repository;
+	}
 
-  @PutMapping("/api/games/{gameid}")
-  void launchRequest(@RequestBody ObjectNode sessionInfo, @PathVariable Long gameid) {
+	@PutMapping("/api/games/{gameid}")
+	void launchRequest(@RequestBody ObjectNode sessionInfo, @PathVariable Long gameid) {
 
-    // TODO: parse the players argument then change the game constructor not to
-    // accept null
-    System.out.println(sessionInfo);
-    // TODO: parse the players to get their names
-    System.out.println("players: " + sessionInfo.get("players").get(0));
-    // Game newGame = new Game(creator, savegame, gameid, players);
-    // save created game object to repository
-    // repository.save(newGame);
-    // LOGGER.info("CREATED: " + newGame); // sending log info
-  }
+		// TODO: parse the players argument then change the game constructor not to
+		// accept null
+		System.out.println(sessionInfo);
+		// TODO: parse the players to get their names
+		System.out.println("players: " + sessionInfo.get("players").get(0));
+		// Game newGame = new Game(creator, savegame, gameid, players);
+		// save created game object to repository
+		// repository.save(newGame);
+		// LOGGER.info("CREATED: " + newGame); // sending log info
+	}
 
-  /**
-   * Quit request.
-   *
-   * @param gameid The game id
-   */
-  @DeleteMapping("/api/games/{gameid}")
-  public void quitRequest(@PathVariable Long gameid) {
-    Game game = repository.findById(gameid).orElseThrow(() -> new GameNotFoundException(gameid));
-    repository.deleteById(gameid);
-    LOGGER.info("DELETED GAME ID: " + gameid);
-  }
+	/**
+	 * Quit request.
+	 *
+	 * @param gameid The game id
+	 */
+	@DeleteMapping("/api/games/{gameid}")
+	public void quitRequest(@PathVariable Long gameid) {
+		Game game = repository.findById(gameid).orElseThrow(() -> new GameNotFoundException(gameid));
+		repository.deleteById(gameid);
+		LOGGER.info("DELETED GAME ID: " + gameid);
+	}
 
-  /*
-   * TODO: encoding for the game state. Create initial for when the LS contacts
-   * server -> send to all users in the session via get
-   */
+	/*
+	 * TODO: encoding for the game state. Create initial for when the LS contacts
+	 * server -> send to all users in the session via get
+	 */
 
-  /**
-   * Ends a player's turn and returns the player whose turn it is.
-   *
-   * @param gameid The game id
-   * @param username The player's username
-   * @return The player whose turn it is
-   */
-  @PutMapping("api/games/{gameid}/{username}")
-  public PlayerWrapper endTurn(@PathVariable(name = "gameid") Long gameid,
-                               @PathVariable(name = "username") String username) {
-    // access the game object from database and then update the reference to the
-    // player not their turn
-    Game game = repository.findById(gameid).orElseThrow(() -> new GameNotFoundException(gameid));
-    updateGameBoard = true;
-    return game.endTurn();
-  }
+	@PutMapping("api/games/{gameid}/gameboard")
+	public void setGameBoard(@PathVariable(name = "gameid") Long gameid, @RequestBody String gameBoard) {
+		// access the game object from database and then update the reference to the
+		// game-board
+		Game game = repository.findById(gameid).orElseThrow(() -> new GameNotFoundException(gameid));
+		game.updateGameBoard(gameBoard);
+		repository.saveAndFlush(game);
+		updateGameBoard = true;
+	}
 
-  /**
-   * Returns the current gameboard.
-   *
-   * @param gameid The game id
-   * @return the current gameboard
-   */
-  @GetMapping("api/games/{gameid}")
-  public DeferredResult<GameBoard> currentGameBoard(@PathVariable Long gameid) {
-    DeferredResult<GameBoard> updatedGameBoard = new DeferredResult<>();
-    updaters.execute(() -> {
-      while (!updateGameBoard) {
+	/**
+	 * Updates the last action of the specified game, flags for game update to be
+	 * sent
+	 * 
+	 * @param gameid
+	 * @param action
+	 */
+	@PutMapping("api/games/{gameid}/endturn")
+	public void endTurn(@PathVariable(name = "gameid") Long gameid, @RequestBody String action) {
+		Game game = repository.findById(gameid).orElseThrow(() -> new GameNotFoundException(gameid));
+		game.setLastAction(action);
+		repository.saveAndFlush(game);
+		updateAction = true;
+	}
 
-      }
-      // TODO: implement
-      updatedGameBoard.setResult(null);
-      updateGameBoard = false;
-    });
+	@GetMapping("api/games/{gameid}/endturn")
+	public DeferredResult<String>getLastAction(@PathVariable(name = "gameid") Long gameid) {
+		DeferredResult<String> updatedAction = new DeferredResult<>();
+		updaters.execute(() -> {
+			while (!updateAction) {
+				try {
+					Thread.sleep(5000);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+			Game game = repository.findById(gameid).orElseThrow(() -> new GameNotFoundException(gameid));
+			updatedAction.setResult(game.getLastAction());
+			updateAction = false;
+		});
+		return updatedAction;
+	}
 
-    return updatedGameBoard;
+	/**
+	 * Returns the current gameboard.
+	 *
+	 * @param gameid The game id
+	 * @return the current gameboard
+	 */
+	@GetMapping("api/games/{gameid}/gameboard")
+	public DeferredResult<String> currentGameBoard(@PathVariable Long gameid) {
+		DeferredResult<String> updatedGameBoard = new DeferredResult<>();
+		updaters.execute(() -> {
+			while (!updateGameBoard) {
+				// just wait
+				try {
+					Thread.sleep(5000);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+			// TODO: implement
+			Game game = repository.findById(gameid).orElseThrow(() -> new GameNotFoundException(gameid));
+			updatedGameBoard.setResult(game.getGameBoard());
+			updateGameBoard = false;
+		});
 
-  }
+		return updatedGameBoard;
 
-  @GetMapping("/api/knock")
-  String knock() {
-    LOGGER.info("SOMEONE'S KNOCKING");
-    return "SOMEONE'S KNOCKING";
-  }
+	}
+
+	@GetMapping("/api/knock")
+	String knock() {
+		LOGGER.info("SOMEONE'S KNOCKING");
+		return "SOMEONE'S KNOCKING";
+	}
 
 }
