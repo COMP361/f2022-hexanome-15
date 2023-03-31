f6e2f35e011d64b37fabf5d85efb7e7fe7ce0a7a
lawrenceberardelli <lawrence.berardelli@gmail.com>
2023-03-31 14:27:01 -0400

    fixing tests

--- server/src/main/java/ca/mcgill/splendorserver/control/AuthTokenAuthenticator.java
+++ server/src/main/java/ca/mcgill/splendorserver/control/AuthTokenAuthenticator.java
@@ -12,7 +12,7 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public final class AuthTokenAuthenticator {
-  private static final String lobbyServiceLocation = "localhost:4242";
+  private static final String lobbyServiceLocation = "http://localhost:4242";
   private static final String GET_USERNAME         = "/oauth/username";
   private static final String GET_ROLE             = "/oauth/role";
 
