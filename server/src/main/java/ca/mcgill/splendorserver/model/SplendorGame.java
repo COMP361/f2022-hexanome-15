8b12f30b3d7d891e1fab7511a7ab37360f246c22
Sofia Fiore <sofia.fiore@mail.mcgill.ca>
2023-04-03 15:51:50 -0400

    Added some comments for instantiating savegames

--- server/src/main/java/ca/mcgill/splendorserver/model/SplendorGame.java
+++ server/src/main/java/ca/mcgill/splendorserver/model/SplendorGame.java
@@ -361,6 +361,8 @@ public class SplendorGame {
     if (sessionInfo.getGameServer().equals("SplendorOrientTradingPosts")) {
       tradingPostSlots = TradingPostSlot.getTradingPostSlots();
     } else if (sessionInfo.getGameServer().equals("SplendorOrientCities")) {
+      // Removing the cities that are in the inventories
+      // to instantiate the gameboard only with cities on the board
       List<Noble> citiesInInventories = new ArrayList<>();
       for (InventoryJson inventory : gameboardJson.inventories) {
         for (Integer cityId : inventory.cities) {
@@ -372,6 +374,8 @@ public class SplendorGame {
       }
       cities.removeAll(citiesInInventories);
     }
+    // Removing the nobles that are in the inventories
+    // to instantiate the gameboard only with nobles on the board
     List<Noble> noblesInInventories = new ArrayList<>();
     for (InventoryJson inventory : gameboardJson.inventories) {
       for (Integer nobleId : inventory.visitingNobles) {
