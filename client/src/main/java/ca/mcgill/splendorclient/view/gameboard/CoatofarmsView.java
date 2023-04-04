758c89fa605ee2a3062a0a73416f61466cc4a031
SlavaSklokin <Slava.Sklokin@outlook.com>
2023-04-03 23:33:16 -0400

    trading posts adaptive layout

--- client/src/main/java/ca/mcgill/splendorclient/view/gameboard/CoatofarmsView.java
+++ client/src/main/java/ca/mcgill/splendorclient/view/gameboard/CoatofarmsView.java
@@ -20,8 +20,8 @@ public class CoatofarmsView extends Rectangle {
    * @param screenSize the size of the screen
    * @param color a string representing the color of the shield
    */
-  public CoatofarmsView(Dimension screenSize, String color) {
-    super(screenSize.getWidth() / 65f, screenSize.getHeight() / 40.f);
+  public CoatofarmsView(Dimension screenSize, float x, float y, String color) {
+    super(x, y);
     Image newImage = new Image("file:///" + rootPath + "/resources/shield_"
                                    + color.toLowerCase() + ".png");
     if (newImage != null) {
